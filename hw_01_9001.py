3732e0de2b1d03530ccee4cdd60242d8a34e3b15
jennylihan <jlhan@stanford.edu>
2019-11-22 14:34:08 +0800

    added test and updated homework

--- hw_01_9001.py
+++ hw_01_9001.py
@@ -1,55 +1,90 @@
-# Data structures practice 0
+# HW 1.90001 Lists Practice
 # Author: ___________
 
 def mean(values):
     """
-    Returns the mean (average) of the values. 
+    Returns the mean (average) of a list of values.
         >>> mean([1, 3, 5])
         3
         >>> mean(range(100))
         49.5
+    Hint: you've done this problem before in HW 1.2...Feel free to adapt your code.
     """
     return 0
 
-def span(values):
+def create_ones_list(length):
     """
-    Returns the distance between the largest and the smallest value 
-    in `values`.
-        >>> span([40, 30, 20, 10])
-        30
-        >>> span(range(100))
-        99
+    Creates a list that has `length` number of elements, and each element is the integer 1.
+    Returns the list.
+        >>> create_ones_list(4)
+        [1,1,1,1]
+        >>> create_ones_list(15)
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+        >>> create_ones_list(0)
+        []
     """
     return 0
 
-def get_stats(values):
-    """ 
-    Returns a dict of statistics describing `values`.
-        >>> stats([1, 2, 2, 7])
-        {
-            "max": 7, 
-            "min": 1, 
-            "len": 4,
-            "mean": 3, 
-        }
+def is_even(values):
     """
-    return {
-        "max": 0,
-        "min": 0,
-        "len": 0,
-        "span": 0,
-        "mean": 0,
-    }
+    Returns True if the length of the list is even. Returns False otherwise.
 
-def count_value(values, target):
+    input: list of any type (values)
+    output: boolean
+
+        >>> is_even(["a", "b", "c"])    def test_count_value_1(self):
+        self.assertEqual(test_count_value_1([1, 1, 1, 1, 2, 2, 2, 3, 3, 4], 2), 3)
+        self.assertEqual(test_count_value_1([1, 1, 1, 1, 2, 2, 2, 3, 3, 4], 3), 2)
+        self.assertEqual(test_count_value_1([], 1), 0)
+        False
+        >>> is_even([1,1,1,2,4,5])
+        True
+
+    By the way, you'll use this for box and whisker plot lab.
+    """
+    return False
+
+def make_even(string_list):
+    """
+    If the list is even, return string_list without changing anything.
+    If the list is not even, append the string "SIKE" to the end of string_list, then return the string_list.
+
+    input: list of strings (string_list)
+    output: list of strings (string_list)
+
+    >>> make_even(["monday", "tuesday", "wednesday"])
+    ["monday", "tuesday", "wednesday", "SIKE"]
+
+    >>> make_even(["7", "8", "9", "10"])
+    ["7", "8", "9", "10"]
+
+    Hint: you can use is_even() inside this function ...
+    """
+    return 0
+
+def count_value_1(values, target):
     """
     Counts how many times `target` appears in `values` and returns an int.
-        >>> values = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4] 
+    input: a list of any type (values) and an int/float (target)
+    output: an int
+        >>> values = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
         >>> count_value(values, 2)
         3
         >>> count_value(values, 3)
         2
         >>> count_value([], 3)
         0
+    """
+    return 0
+
+def count_value_2(values, target):
+    """
+    You wrote the function above using one type of for-loop.
+    Now write the exact same function as above, except use the other type of for-loop.
+
+    Hint: What are the two types of for-loops, you ask? Check out the Unit 0 Loops Lab.
+    Or, here's a hint:
+    1. for _ in range(___):
+    2. for _ in ___:
     """
     return 0
---
+++ test_homework.py
@@ -0,0 +1,50 @@
+# Unit 1 Homework 9001 Test script
+# Author: Chris Proctor and Jenny Han
+# --------------------
+# YOU DO NOT NEED TO UNDERSTAND THIS CODE RIGHT NOW!
+# This script imports all the functions from your homework and tests them out. Hopefully they work!
+# Testing is really important in computer science, so some nice functions are provided. We'll use them.
+
+import unittest
+
+from hw_01_9001 import *
+
+class TestHomework_01_9000(unittest.TestCase):
+
+    def test_mean(self):
+        self.assertEqual(mean([1, 2, 3, 4, 5]), 3.0)
+        self.assertEqual(mean([1]), 1.0)
+        self.assertEqual(mean([0]), 0.0)
+        self.assertEqual(mean([2, 6, 3, 11]), 5.5)
+
+
+    def test_create_ones_list(self):
+        self.assertEqual(create_ones_list(1), [1])
+        self.assertEqual(create_ones_list(0), [])
+        self.assertEqual(create_ones_list(5), [1,1,1,1,1])
+        self.assertEqual(create_ones_list(50), [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,])
+
+    def test_is_even(self):
+        self.assertEqual(is_even([1, 2, 3]), False)
+        self.assertEqual(is_even([1, 2, 3, 4]), True)
+        self.assertEqual(is_even([]), True)
+        self.assertEqual(is_even([1]), False)
+
+    def test_make_even(self):
+        self.assertEqual(make_even(["hi"]), ["hi", "SIKE"])
+        self.assertEqual(make_even(["1", "2"]),["1", "2"] )
+        self.assertEqual(make_even(["say", "right", "now"]),["say", "right", "now","SIKE"] )
+        self.assertEqual(make_even(["say", "sike", "right", "now"]),["say", "sike", "right", "now"] )
+
+    def test_count_value_1(self):
+        self.assertEqual(test_count_value_1([1, 1, 1, 1, 2, 2, 2, 3, 3, 4], 2), 3)
+        self.assertEqual(test_count_value_1([1, 1, 1, 1, 2, 2, 2, 3, 3, 4], 3), 2)
+        self.assertEqual(test_count_value_1([], 1), 0)
+
+    def test_count_value_2(self):
+        self.assertEqual(test_count_value_2([1, 1, 1, 1, 2, 2, 2, 3, 3, 4], 2), 3)
+        self.assertEqual(test_count_value_2([1, 1, 1, 1, 2, 2, 2, 3, 3, 4], 3), 2)
+        self.assertEqual(test_count_value_2([], 1), 0)
+
+# This runs all the tests.
+unittest.main(verbosity=2)
